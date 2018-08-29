ee84a83cf99b5ea0ee43425546e608c56974a36f
Jayant Mehra <mehrajayant@gmail.com>
2018-08-29 10:59:18 +0530

    Initial Commit

---
+++ DependencyManager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DependencyManager
+//
+//  Created by Jayant Mehra on 8/29/18.
+//  Copyright © 2018 Jayant Mehra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
